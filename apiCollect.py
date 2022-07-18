a308d5c356868a92cdf089413ebf9e6cffeb269c
Joshua Levy <joshua.levy@chicagobooth.edu>
2022-07-18 16:43:04 -0500

    Get all episodes logic
    Completed the basic API calls that collects the episodes (with their release dates and, critically, unique IDs).
    
    Still need to do some slight tweaking of the logic in re checking the existing .csv file and extracting the last-collected time.

--- apiCollect.py
+++ apiCollect.py
@@ -1,24 +1,99 @@
 import pandas as pd
 import requests
+import time
 import json
+from datetime import datetime
+
 
 ### READ IN API KEY
 api_keys_dict = json.load(open('env_keys.json'))
 simplecast_key = api_keys_dict.get('simplecast_key')
-
-
 auth_headers = {
     'authorization' : 'Bearer {}'.format(simplecast_key)
 }
 
 
-#url = 'https://api.simplecast.com/podcasts'
-
-podcast_id = '912f8fce-96d2-4583-92f5-2279c08e377a'
-
-url = 'https://api.simplecast.com/analytics/episodes?podcast='+podcast_id
+### GLOBAL VARS OF INTEREST
+capitalisnt_podcast_id = '912f8fce-96d2-4583-92f5-2279c08e377a'
+today = datetime.now()
 
 
-response = requests.get(url, headers=auth_headers)
-print(response.text)
 
+
+def getNext(next_url):
+    next_response = requests.get(next_url, headers=auth_headers)
+    next_response_json = next_response.json()
+
+    now_current_collection = next_response_json.get('collection')
+    now_current_page_num = next_response_json.get('pages').get('current')
+    now_next_page_url = next_response_json.get('pages').get('next').get('href')
+    
+    return now_current_collection, now_current_page_num, now_next_page_url  
+
+def getAllEpisodes(current_datetime):
+    get_eps_params = {
+        'podcast' : capitalisnt_podcast_id
+    }
+    url = 'https://api.simplecast.com/analytics/episodes'
+
+    ### TODO: change this out with a regex interpretation of a .csv OR 
+    last_collected_datetime = datetime.strptime('2022-06-14', '%Y-%m-%d')
+    #####
+    time_since_collection = current_datetime - last_collected_datetime
+    
+    eps_response = requests.get(url, headers=auth_headers, params=get_eps_params)
+    r = eps_response.json()
+
+    eps_collection = r.get('collection')
+    episodes_list = eps_collection
+
+    pages_dict = r.get('pages')
+    expected_pages = pages_dict.get('total')
+    current_page = pages_dict.get('current')
+    next_page_url = pages_dict.get('next').get('href')
+
+    print('Expected # of episode pages: {}'.format(expected_pages))
+
+    if time_since_collection.days < 20:
+        # SOME OTHER LOGIC ACCORDING TO READING THE CURRENT DATA FRAME, OVERWRITING AND DROPPING DUPLICATES
+        old_df = pd.read_csv()
+
+        new_df = pd.DataFrame.from_dict(episodes_list)
+        new_df['season'] = new_df['season'].apply(lambda obj: obj.get('number'))
+        new_df['season-ep'] = new_df.apply(lambda row: 'S{}-EP{}'.format(row.season, row.number), axis=1)
+
+        new_df.rename(columns={
+            'href' : 'episode_download_href',
+            'id' : 'episode_id'
+        }, inplace=True)
+        new_df.drop(columns=['season', 'number', 'downloads'], inplace=True)
+
+        df = pd.concat([old_df, new_df])
+        df = df.drop_duplicates()
+
+        return df
+    else:
+
+        ### TODO:  CHANGE THIS TO 'expected_pages' WHEN WE ARE READY TO GO
+        while current_page < 3:
+            now_current_collection, current_page, next_page_url = getNext(next_page_url)
+            print('Just collected {}; about to collect {} out of {} pages'.format(current_page -1, current_page, expected_pages))
+
+            episodes_list += now_current_collection
+
+        print('Finished collection {} out of {} pages'.format(current_page, expected_pages))
+        df = pd.DataFrame.from_dict(episodes_list)
+        df['season'] = df['season'].apply(lambda obj: obj.get('number'))
+        df['season-ep'] = df.apply(lambda row: 'S{}-EP{}'.format(row.season, row.number), axis=1)
+
+        df.rename(columns={
+            'href' : 'episode_download_href',
+            'id' : 'episode_id'
+        }, inplace=True)
+        df.drop(columns=['season', 'number', 'downloads'], inplace=True)
+
+        return df
+
+
+x = getAllEpisodes()
+x
