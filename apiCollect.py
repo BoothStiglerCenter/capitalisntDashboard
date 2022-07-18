09fb997d95015d7879760769f01c6cb62cdd03c6
Joshua Levy <joshua.levy@chicagobooth.edu>
2022-07-18 14:09:01 -0500

    Setting up local API key infra

--- apiCollect.py
+++ apiCollect.py
@@ -2,9 +2,23 @@ import pandas as pd
 import requests
 import json
 
+### READ IN API KEY
+api_keys_dict = json.load(open('env_keys.json'))
+simplecast_key = api_keys_dict.get('simplecast_key')
 
-# api_keys_dict = json.load(open())
-# simplecast_key = api_keys_dict.get('')
+
+auth_headers = {
+    'authorization' : 'Bearer {}'.format(simplecast_key)
+}
 
 
-request = requests.get()
+#url = 'https://api.simplecast.com/podcasts'
+
+podcast_id = '912f8fce-96d2-4583-92f5-2279c08e377a'
+
+url = 'https://api.simplecast.com/analytics/episodes?podcast='+podcast_id
+
+
+response = requests.get(url, headers=auth_headers)
+print(response.text)
+
