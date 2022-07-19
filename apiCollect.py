7f32c10f790f408da2259f76cb11424d92fe7211
Joshua Levy <joshua.levy@chicagobooth.edu>
2022-07-19 16:24:14 -0500

    Identifying previously collected .csvs
    Generalized a function identifyExistingCollection() that will help to identify if there already exists an old .csv and if so, returns that .csv and the date it was collected..
    -- Modified getAllEpisodes() to work with this new generalized check function
    
    Getting started on a function (getEpDownloads()) that collects daily download data for each episode.

--- apiCollect.py
+++ apiCollect.py
@@ -14,28 +14,58 @@ auth_headers = {
     'authorization' : 'Bearer {}'.format(simplecast_key)
 }
 
+### GLOBAL VARS OF INTEREST
+capitalisnt_podcast_id = '912f8fce-96d2-4583-92f5-2279c08e377a'
+today = datetime.now().date()
+files_in_dir = os.listdir()
+date_format = '%Y-%m-%d'
+date_pattern  = r'\d{4}-\d{2}-\d{2}'
+default_date = datetime.strptime('2000-01-01', date_format).date()
 
 class MismatchError(Exception):
     """ 
         Exception raised when two values are not the same
         Particularly useful when auditing the length of something
     """
-    def __init__(self, lhs, rhs, message='Two values do not match. Try resetting and starting again'):
+    def __init__(self, lhs, rhs, message=""):
         self.lhs = lhs
         self.rhs = rhs
         self.message='Two values do not match (LHS: {}   RHS: {})'.format(lhs, rhs)
         super().__init__(self.message)
 
 
+### Identifies whether a certain type of .csv has already been collected (ie is in this folder somewhere)
+def identifyExistingCollection(current_datetime, type_csv_search_pattern):
 
-### GLOBAL VARS OF INTEREST
-capitalisnt_podcast_id = '912f8fce-96d2-4583-92f5-2279c08e377a'
-today = datetime.now()
-files_in_dir = os.listdir()
+    # Establishes patterns so that this function is generalizable to lots of types of .csvs
+    patterns_dict = {
+        'episodes_core' : r'^episodes_core',
+        'episodes_downloads' : r'^episodes_downloads'
+    }
+
+    
+    pattern = patterns_dict.get(type_csv_search_pattern)
+    # Sets the default last-collected date. If no files match the desired pattern, this is the date that's returned.
+    last_collected_datetime = default_date
 
 
+    for filename in files_in_dir:
+        if re.search(pattern, filename):
+            last_collected_datetime_string = re.search(date_pattern, filename)[0]
+            last_collected_datetime = datetime.strptime(last_collected_datetime_string, date_format).date()
+            break
 
+    # If we've done some kind of collection before, load in the old one and pass the date it was last collected back to the function that called this. Otherwise, pass back the default date (2000/01/01) and note that we have never collected this type of df before.
+    if default_date < last_collected_datetime: 
+        for i, filename in enumerate(files_in_dir):
+            if re.search(pattern, filename):
+                old_df_path = files_in_dir[i]
+                break
+        collection = pd.read_csv(old_df_path)
+    elif last_collected_datetime == default_date:
+        collection = 'not previously collected'
 
+    return collection, last_collected_datetime
 
 
 def getNext(next_url):
@@ -54,15 +84,14 @@ def getAllEpisodes(current_datetime):
     }
     url = 'https://api.simplecast.com/analytics/episodes'
 
-    for filename in files_in_dir:
-        if re.search(r'^episodes_core', filename):
-            last_collected_datetime_string = re.search(r'\d{4}-\d{2}-\d{2}', filename)[0]
-            last_collected_datetime = datetime.strptime(last_collected_datetime_string, '%Y-%m-%d')
+
+    old_df, last_collected_datetime = identifyExistingCollection(current_datetime, 'episodes_core')
+
+    if type(old_df) == str:
+        old_df = pd.DataFrame()
  
-    try:
-       time_since_collection = current_datetime - last_collected_datetime
-    except:
-        time_since_collection = current_datetime - datetime.strptime('2000-01-01', '%Y-%m-%d')
+    time_since_collection = current_datetime - last_collected_datetime
+
 
     eps_response = requests.get(url, headers=auth_headers, params=get_eps_params)
     r = eps_response.json()
@@ -78,13 +107,12 @@ def getAllEpisodes(current_datetime):
 
     print('Expected # of episode pages: {}'.format(expected_pages))
 
-    if time_since_collection.days < 20:
-        # SOME OTHER LOGIC ACCORDING TO READING THE CURRENT DATA FRAME, OVERWRITING AND DROPPING DUPLICATES
-        old_df = pd.read_csv()
+    # If we have collected within the past 50 days, only call the API once to get the most recent episode IDs. Otherwise, we can't really count on how many observations we have so collect every observation that you can get out of this API endpoint 'analytics/episodes'
+    if time_since_collection.days < 50:
 
         new_df = pd.DataFrame.from_dict(episodes_list)
         new_df['season'] = new_df['season'].apply(lambda obj: obj.get('number'))
-        new_df['season-ep'] = new_df.apply(lambda row: 'S{}-EP{}'.format(row.season, row.number), axis=1)
+        new_df['season-ep'] = new_df.apply(lambda row: 'S{}-EP{}'.format(int(row.season), int(row.number)), axis=1)
 
         new_df.rename(columns={
             'href' : 'episode_download_href',
@@ -93,15 +121,15 @@ def getAllEpisodes(current_datetime):
         new_df.drop(columns=['season', 'number', 'downloads'], inplace=True)
 
         df = pd.concat([old_df, new_df])
-        df = df.drop_duplicates()
+        df = df.drop_duplicates(subset=['episode_download_href'])
 
         if len(df) != expected_episodes:
             raise MismatchError(len(df), expected_episodes)
 
         return df
+    
     else:
-
-        ### TODO:  CHANGE THIS TO 'expected_pages' WHEN WE ARE READY TO GO
+        # The API only returns 5 episodes at a time so we need to keep calling until we get all 'expected_pages' pages. 
         while current_page < expected_pages:
             now_current_collection, current_page, next_page_url = getNext(next_page_url)
             print('Just collected {}; about to collect {} out of {} pages'.format(current_page -1, current_page, expected_pages))
@@ -111,7 +139,7 @@ def getAllEpisodes(current_datetime):
         print('Finished collection {} out of {} pages'.format(current_page, expected_pages))
         df = pd.DataFrame.from_dict(episodes_list)
         df['season'] = df['season'].apply(lambda obj: obj.get('number'))
-        df['season-ep'] = df.apply(lambda row: 'S{}-EP{}'.format(row.season, row.number), axis=1)
+        df['season-ep'] = df.apply(lambda row: 'S{}-EP{}'.format(int(row.season), int(row.number)), axis=1)
 
         df.rename(columns={
             'href' : 'episode_download_href',
@@ -119,7 +147,7 @@ def getAllEpisodes(current_datetime):
         }, inplace=True)
         df.drop(columns=['season', 'number', 'downloads'], inplace=True)
 
-        episodes_core_out_path = 'episodes_core-{}.csv'.format(datetime.strftime(current_datetime), '%Y-%m-%d')
+        episodes_core_out_path = 'episodes_core-{}.csv'.format(datetime.strftime(current_datetime), date_format)
         if len(df) != expected_episodes:
             raise MismatchError(len(df), expected_episodes)
         df.to_csv(episodes_core_out_path, index=False, encoding='utf-8')
@@ -127,6 +155,57 @@ def getAllEpisodes(current_datetime):
         return df
 
 
-x = getAllEpisodes(today)
 
-x
+
+
+def getEpDownloads(current_datetime):
+    get_downloads_params = {
+        'interval' : 'day',
+    }
+
+    eps_df, last_collected_datetime = identifyExistingCollection(current_datetime, 'episodes_core')
+
+    if type(eps_df) == str:
+        eps_df = getAllEpisodes(current_datetime, last_collected_datetime)
+    eps_df = eps_df[['episode_download_href', 'title', 'episode_id', 'season-ep']]
+    
+
+    eps_downloads_df, downloads_last_collected_datetime = identifyExistingCollection(current_datetime, 'episodes_downloads')
+
+    # If we've never collected episode downloads before, we got a string back from identifyExistingCollection() and we nee to initialize a df before downloading everything.
+    if type(eps_downloads_df) == str:
+        eps_downloads_df = pd.DataFrame()
+    
+    # If we have collected episode downloads before we only want to download new dates (we pass this this to the API)
+    if default_date < downloads_last_collected_datetime:
+        get_downloads_params['start_date'] = downloads_last_collected_datetime.isoformat()
+
+    eps_df = eps_df.head(5)
+    for i, obs in eps_df.iterrows():
+
+        episode_downloads_url = obs.episode_download_href
+        episode_id = obs.episode_id
+        episode_title = obs.title
+
+        response = requests.get(episode_downloads_url,
+            headers = auth_headers,
+            params = get_downloads_params)
+
+        downloads = response.json().get('by_interval')
+        downloads_df = pd.DataFrame.from_dict(downloads)
+        downloads_df['episode_id'] = episode_id
+        downloads_df['title'] = episode_title
+
+        eps_downloads_df = pd.concat([eps_downloads_df, downloads_df], ignore_index=True)
+
+    eps_downloads_df = eps_downloads_df.drop_duplicates(subset=['episode_id', 'interval'])
+    print('trying to get new eps downloads')
+
+
+
+
+    return eps_downloads_df
+
+
+
+getEpDownloads(today)
